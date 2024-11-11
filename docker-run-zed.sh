bd4f7008f8a6e519172bc50a89acbaf9c2ebeaed
thien tran <thientran2032@outlook.com>
2024-11-11 14:13:49 +1100

    Initial commit

---
+++ docker-run-zed.sh
@@ -0,0 +1,31 @@
+DOCKERFILE=Dockerfile.zed
+IMAGE_NAME=utsma/zed
+CONTAINER_NAME=zed_container
+
+# Allow X11 access for the container
+xhost +local:root
+
+# Remove old containers
+docker ps -a | grep $IMAGE_NAME | awk '{print $1}' | xargs -r docker rm
+
+# Remove old images
+docker rmi -f $IMAGE_NAME
+docker rm -f $CONTAINER_NAME
+
+# Build the Docker image
+docker build -t $IMAGE_NAME -f $DOCKERFILE .
+
+# Run the Docker container
+docker run -it --runtime=nvidia \
+   --name $CONTAINER_NAME \
+   --privileged \
+   --network host \
+   --device /dev/video0 \
+   --volume /tmp/argus_socket:/tmp/argus_socket \
+   --env DISPLAY=$DISPLAY \
+   --env NVIDIA_DRIVER_CAPABILITIES=all \
+   --env NVIDIA_VISIBLE_DEVICES=all \
+   --env QT_X11_NO_MITSHM=1 \
+   --volume /tmp/.X11-unix:/tmp/.X11-unix \
+   --volume /home/utsma/zed_sdk_docker/sdk_config:/usr/local/zed/resources \
+   $IMAGE_NAME
